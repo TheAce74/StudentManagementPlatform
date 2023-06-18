6e16b1af9594f3a9ce6d63c94225b08d0e17c91e
The Ace <udonsichisom02@gmail.com>
2023-06-18 15:28:58 +0100

    style: finished the styling for the dashboard

--- src/pages/dashboard/SharedDashboard.jsx
+++ src/pages/dashboard/SharedDashboard.jsx
@@ -5,40 +5,56 @@ import { FaUser, FaBell } from "react-icons/fa";
 import { IoMail } from "react-icons/io5";
 import { BsFillClipboard2DataFill } from "react-icons/bs";
 import { FiLogOut } from "react-icons/fi";
+import { AiOutlineMenu } from "react-icons/ai";
+import { CgClose } from "react-icons/cg";
+import { useState } from "react";
 
 function SharedDashboard() {
   const { state } = useLocation();
 
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
   return (
     <div className="dashboard-grid">
-      <div>
+      <button className="menu" onClick={handleOpen}>
+        {!open ? <AiOutlineMenu /> : <CgClose />}
+      </button>
+      <div className={open ? "open" : ""}>
         <div className="dashboard-grid__logo">
           <img src={logo} alt="" />
           <h2>FUTO</h2>
         </div>
         <div>
-          <Link to="/dashboard" state={state}>
+          <Link to="/dashboard" state={state} data-title="Dashboard">
             <MdSpaceDashboard />
             <span>Dashboard</span>
           </Link>
-          <Link to="/dashboard/profile" state={state}>
+          <Link to="/dashboard/profile" state={state} data-title="Profile">
             <FaUser />
             <span>Profile</span>
           </Link>
-          <Link to="/dashboard/messages" state={state}>
+          <Link to="/dashboard/messages" state={state} data-title="Messages">
             <IoMail />
             <span>Messages</span>
           </Link>
-          <Link to="/dashboard/notifications" state={state}>
+          <Link
+            to="/dashboard/notifications"
+            state={state}
+            data-title="Notifications"
+          >
             <FaBell />
             <span>Notifications</span>
           </Link>
-          <Link to="/dashboard/results" state={state}>
+          <Link to="/dashboard/results" state={state} data-title="Results">
             <BsFillClipboard2DataFill />
             <span>Results</span>
           </Link>
         </div>
-        <Link to="/" className="logout">
+        <Link to="/" className="logout" data-title="Logout">
           <FiLogOut />
           <span>Logout</span>
         </Link>
