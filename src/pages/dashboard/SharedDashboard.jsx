8b9379afc07835a74db59839ed2cc8487756ddce
Onuoha Cherish <writetoamablac@gmail.com>
2023-06-23 07:54:27 +0100

    style: added some little extra styling

--- src/pages/dashboard/SharedDashboard.jsx
+++ src/pages/dashboard/SharedDashboard.jsx
@@ -13,6 +13,7 @@ function SharedDashboard() {
   const { state } = useLocation();
 
   const [open, setOpen] = useState(false);
+  const [currentLink, setCurrentLink] = useState(0);
 
   const handleOpen = () => {
     setOpen(!open);
@@ -20,6 +21,10 @@ function SharedDashboard() {
 
   const handleLogOut = () => {
     sessionStorage.removeItem("key");
+  };
+
+  const handleChangeCurrentLink = (value) => {
+    setCurrentLink(value);
   };
 
   return (
@@ -33,15 +38,33 @@ function SharedDashboard() {
           <h2>FUTO</h2>
         </div>
         <div>
-          <Link to="/dashboard" state={state} data-title="Dashboard">
+          <Link
+            to="/dashboard"
+            state={state}
+            data-title="Dashboard"
+            className={currentLink === 0 ? "active" : ""}
+            onClick={() => handleChangeCurrentLink(0)}
+          >
             <MdSpaceDashboard />
             <span>Dashboard</span>
           </Link>
-          <Link to="/dashboard/profile" state={state} data-title="Profile">
+          <Link
+            to="/dashboard/profile"
+            state={state}
+            data-title="Profile"
+            className={currentLink === 1 ? "active" : ""}
+            onClick={() => handleChangeCurrentLink(1)}
+          >
             <FaUser />
             <span>Profile</span>
           </Link>
-          <Link to="/dashboard/messages" state={state} data-title="Messages">
+          <Link
+            to="/dashboard/messages"
+            state={state}
+            data-title="Messages"
+            className={currentLink === 2 ? "active" : ""}
+            onClick={() => handleChangeCurrentLink(2)}
+          >
             <IoMail />
             <span>Messages</span>
           </Link>
@@ -49,11 +72,19 @@ function SharedDashboard() {
             to="/dashboard/notifications"
             state={state}
             data-title="Notifications"
+            className={currentLink === 3 ? "active" : ""}
+            onClick={() => handleChangeCurrentLink(3)}
           >
             <FaBell />
             <span>Notifications</span>
           </Link>
-          <Link to="/dashboard/results" state={state} data-title="Results">
+          <Link
+            to="/dashboard/results"
+            state={state}
+            data-title="Results"
+            className={currentLink === 4 ? "active" : ""}
+            onClick={() => handleChangeCurrentLink(4)}
+          >
             <BsFillClipboard2DataFill />
             <span>Results</span>
           </Link>
